9967c52d171efd1614e46d5602995574d3e36d8b
shikoumuzi <1013740747@qq.com>
2022-10-13 21:28:34 +0800

    隐藏敏感信息

--- data/datainit.py
+++ data/datainit.py
@@ -4,11 +4,8 @@ import os
 file = open("user.txt", "a+")
 
 md5 = hashlib.md5("shikoumuziinjiande".encode("utf-8"))
-md5.update("123456".encode("utf-8"))
+md5.update("1234567".encode("utf-8"))
 pwd = md5.hexdigest()
-
-data = ["李文智", pwd]
-file.write("李文智 " + pwd)
 
 file.close()
 
